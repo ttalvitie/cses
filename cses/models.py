9f18695e76083f3c2ef18cec49d8e5824ec27d92
Topi Talvitie <topi.talvitie@iki.fi>
2013-04-05 20:53:59 +0300

    Added cses app.

---
+++ cses/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+class User(models.Model):
+	pass
+
+class Group(models.Model):
+	pass
+
+class Contest(models.Model):
+	pass
+
+class Task(models.Model):
+	pass
+
+class Input(models.Model):
+	pass
+
+class Submission(models.Model):
+	pass
+
+class Result(models.Model):
+	pass
