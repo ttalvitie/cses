565b71dbf2ac5e5ed87a79fb6d41b0c1b445fd41
Mikko Sysikaski <mikko.sysikaski@gmail.com>
2013-06-17 17:25:04 +0300

    Displaying stderr on submission view.

--- cses/admin.py
+++ cses/admin.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 from pygments import highlight
 from pygments.formatters import HtmlFormatter
 from pygments.lexers import guess_lexer_for_filename
+from django.core.urlresolvers import reverse
 import sys
 
 admin.site.register(Language)
@@ -35,12 +36,14 @@ admin.site.register(User, MyUserAdmin)
 
 class ResultInline(admin.TabularInline):
 	model = Result
-	readonly_fields = ('input','output','correct','result', 'time')
+	readonly_fields = ('input','output','correct','result', 'time', 'erroutput')
 	exclude = ('testcase', 'stdout', 'stderr', 'memory')
 	def input(self, instance):
 		return instance.testcase.input.read(1000)
 	def output(self, instance):
 		return instance.stdout.read(1000)
+	def erroutput(self, instance):
+		return instance.stderr.read(1000)
 	def correct(self, instance):
 		return instance.testcase.output.read(1000)
 	input.short_description = 'stdin'
@@ -54,8 +57,9 @@ class ResultInline(admin.TabularInline):
 
 class SubmissionAdmin(admin.ModelAdmin):
 	inlines = [ResultInline]
-	exclude = ('binary','judgeResult')
-	readonly_fields = ('task', 'contest', 'user', 'language', 'time', 'source', 'sourceText', 'compileResult', 'resultString')
+	exclude = ('task','binary','judgeResult')
+	readonly_fields = ('task_', 'contest', 'user', 'language', 'time', 'source', 'sourceText', 'compileResult', 'resultString')
+	list_display_links = ['task']
 
 	def sourceText(self, instance):
 		data = instance.source.read()
@@ -64,6 +68,12 @@ class SubmissionAdmin(admin.ModelAdmin):
 		return highlight(data, lexer, formatter)
 	sourceText.short_description = 'Source'
 	sourceText.allow_tags = True
+
+	def task_(self, obj):
+		ct = obj.task
+		url = reverse('admin:cses_task_change', args=(ct.id,))
+		return '<a href="%s">%s</a>' % (url, ct.name)
+	task_.allow_tags = True
 
 	list_display = ('task','user','language','resultString')
 	list_display_links = list_display
